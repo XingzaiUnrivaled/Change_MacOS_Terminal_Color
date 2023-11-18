00ef4734546ae757472059408d62cba9f5caa9a5
XingZai <44067807+XingzaiUnrivaled@users.noreply.github.com>
2023-11-18 10:06:13 +0800

    上传脚本

---
+++ change_terminal_color.sh
@@ -0,0 +1,3 @@
+cd ~
+echo "export CLICOLOR='Yes'\r\nexport LSCOLORS='ExGxFxdaCxDaDahbadacec'" >> .bash_profile
+echo "export CLICOLOR='Yes'\r\nexport LSCOLORS='ExGxFxdaCxDaDahbadacec'" >> .zshrc
---
+++ default_terminal_color.sh
@@ -0,0 +1,3 @@
+cd ~
+echo "export CLICOLOR='Yes'\r\nexport LS_OPTIONS='--color=auto'" >> .bash_profile
+echo "export CLICOLOR='Yes'\r\nexport LS_OPTIONS='--color=auto'" >> .zshrc
